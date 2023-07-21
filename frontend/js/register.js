ca5657846f8e7a160a2f220a396babd5ee760a61
najm-choueiry <nachoueiry@outlook.com>
2023-07-22 01:55:27 +0300

    creating register.js

---
+++ frontend/js/register.js
@@ -0,0 +1,26 @@
+const submit = document.getElementById("submit")
+
+const base_url = "http://localhost/Assignments/google-classroom-clone/backend/"
+
+submit.addEventListener("click", register)
+
+const register = () => {
+    const first_name = document.getElementById("first_name").value
+    const last_name = document.getElementById("last_name").value
+    const email = document.getElementById("email").value
+    const password = document.getElementById("password").value
+    const recovery_email = document.getElementById("recovery_email").value
+
+    const user_info = new FormData();
+    user_info.append("first_name", first_name)
+    user_info.append("last_name", last_name)
+    user_info.append("email",email)
+    user_info.append("password", password)
+    user_info.append("recovery_email", recovery_email)
+
+    fetch(base_url + 'register.php') {
+        method: "Post",
+        
+    }
+    
+}
