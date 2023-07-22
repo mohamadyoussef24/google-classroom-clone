d92fc173451a93265cd0f1cce8361dc3443a4745
HilalBodon <hilal_bodon@hotmail.com>
2023-07-23 00:00:51 +0300

    adding restricrions to registration page

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -1,42 +1,78 @@
-const submit = document.getElementById("submit")
+const submit = document.getElementById("submit");
+const infoDiv = document.querySelector(".info");
 
+const base_url = "http://localhost/Assignments/google-classroom-clone/backend/";
 
-const base_url = "http://localhost/Assignments/google-classroom-clone/backend/"
+const validateForm = () => {
+    const first_name = document.getElementById("first_name").value;
+    const last_name = document.getElementById("last_name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const recovery_email = document.getElementById("recovery_email").value;
+
+    if (!first_name || !last_name || !email || !password || !recovery_email) {
+        infoDiv.textContent = "All fields are required.";
+        infoDiv.style= 'color: red;font-weight:bold;font-size:14px;';
+        return false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!email.match(emailRegex)) {
+        infoDiv.textContent = "Invalid email format.";
+        infoDiv.style= 'color: red;font-weight:bold;font-size:14px;';
+        return false;
+    }
+
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!password.match(passwordRegex)) {
+        infoDiv.textContent = "Password must be at least 8 characters long and contain letters and numbers.";
+        infoDiv.style= 'color: red;font-weight:bold;font-size:14px;';
+        return false;
+    }
+
+    return true;
+};
 
 const register = () => {
-    const first_name = document.getElementById("first_name").value
-    const last_name = document.getElementById("last_name").value
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    const recovery_email = document.getElementById("recovery_email").value
+    if (!validateForm()) {
+        return;
+    }
 
+
+    const first_name = document.getElementById("first_name").value;
+    const last_name = document.getElementById("last_name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const recovery_email = document.getElementById("recovery_email").value;
+    
     try {
         const user_info = new FormData();
-        user_info.append("first_name", first_name)
-        user_info.append("last_name", last_name)
-        user_info.append("email",email)
-        user_info.append("password", password)
-        user_info.append("recovery_email", recovery_email)
+        user_info.append("first_name", first_name);
+        user_info.append("last_name", last_name);
+        user_info.append("email", email);
+        user_info.append("password", password);
+        user_info.append("recovery_email", recovery_email);
 
-        fetch(base_url + 'register.php' ,{
+        fetch(base_url + 'register.php', {
             method: "POST",
             body: user_info
         }).then((res) => res.json())
         .then((res) => {
-            if(res.status === 'success') {
-                const user_id = res.user_id
+            if (res.status === 'success') {
+                const user_id = res.user_id;
                 localStorage.setItem("user_id", user_id);
-                window.location.replace("../views/classroom_view.html")
+                window.location.replace("../views/classroom_view.html");
+            } else {
+                alert("Registration failed. " + res.message);
             }
         });
 
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
 
-
-submit.addEventListener("click", function(e){
+submit.addEventListener("click", function(e) {
     e.preventDefault();
     register();
-})
+});
