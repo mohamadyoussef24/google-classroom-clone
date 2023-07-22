731a525a083d5c6d32caa825dc9098b10e7ac2af
najm-choueiry <nachoueiry@outlook.com>
2023-07-22 18:33:38 +0300

    email - code sent to user

--- frontend/js/forgot_password.js
+++ frontend/js/forgot_password.js
@@ -11,19 +11,16 @@ submit.addEventListener("click", function(event){
 
 sendEmail = () => {
         const recovery_email = document.getElementById('recovery_email1').value
-    code = Math.random() * 1000000 | 0
+        code = Math.random() * 1000000 | 0
         
     console.log(recovery_email)
-    emailjs.init("ua6aWzLhhQq3fLfQO"); //please encrypted user id for malicious attacks
+    emailjs.init("ua6aWzLhhQq3fLfQO");
 
 
     var templateParams = {
     to_name:  recovery_email,
     from_name: site_email ,
-    message_html: `Please take note of the following code to reset your password:
-    ${code}
-    `
-
+    data: code
     };
 
     emailjs.send('service_nkade5d', 'template_o7g4izb', templateParams)
@@ -45,7 +42,7 @@ proceed.addEventListener('click', function(event){
 checkCode = ()=>{
     const reset_code = document.getElementById('reset_code').value
 
-    if (reset_code === code) {
+    if (reset_code == code.toString()) {
         console.log('success')
         window.location.replace("../views/reseting_password.html")
     }else {
