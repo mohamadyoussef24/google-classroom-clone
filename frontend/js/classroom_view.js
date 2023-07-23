95091fd13899c59bcc0c74f28c33483192f70a02
HilalBodon <hilal_bodon@hotmail.com>
2023-07-23 12:42:52 +0300

    handling the action of burger when press on body menu in

--- frontend/js/classroom_view.js
+++ frontend/js/classroom_view.js
@@ -1,4 +1,15 @@
 var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function(){
-    document.querySelector("body").classList.toggle("active");
-})
+var body = document.querySelector("body");
+var sidebar = document.querySelector(".sidebar");
+
+hamburger.addEventListener("click", function() {
+  body.classList.toggle("active");
+});
+
+body.addEventListener("click", function(event) {
+  if  (hamburger.contains(event.target)){
+    body.classList.remove("active");
+  } else if (!sidebar.contains(event.target) && !hamburger.contains(event.target)) {
+    body.classList.add("active");
+  }
+});
