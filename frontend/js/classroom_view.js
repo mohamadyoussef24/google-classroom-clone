3239d00399159cb09f2dc0d8699368670bb8afe8
najm-choueiry <nachoueiry@outlook.com>
2023-07-23 15:04:13 +0300

    creating classes

--- frontend/js/classroom_view.js
+++ frontend/js/classroom_view.js
@@ -13,3 +13,32 @@ body.addEventListener("click", function(event) {
     body.classList.add("active");
   }
 });
+
+
+const class_options = document.getElementById('class_options')
+const class_options_list = document.getElementById('class_options1')
+
+class_options.addEventListener('click', function(){
+  if (class_options_list.style.display == "none") {
+    class_options_list.style.display = "block";
+  } else {
+    class_options_list.style.display = "none";
+  }
+})
+
+const join_class = document.getElementById('join_class')
+const create_class = document.getElementById('create_class')
+
+
+join_class.addEventListener('click', joinClass)
+create_class.addEventListener('click', createClass)
+
+
+const joinClass = ()=> {
+  
+}
+
+
+const createClass = ()=> {
+  
+}
