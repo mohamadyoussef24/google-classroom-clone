69874079a89c48d9ffab6c2ec5d1ba957c4bcbaf
Alaa Attaya <attayaalaa@gmail.com>
2023-07-24 01:38:04 +0300

    Fix join_class php query

--- backend/join_class.php
+++ backend/join_class.php
@@ -19,13 +19,48 @@ $num_rows = $query->num_rows();
 if ($num_rows == 0) {
     $response['status'] = "class not found";
 } else {
+    $query = $mysqli->prepare('select id
+    from teachers 
+    where class_id=?');
+    $query->bind_param('i', $class_id);
+    $query->execute();
+    $query->store_result();
+    $query->bind_result($class_id1);
+    $query->fetch();
+    $num_rows2 = $query->num_rows();
+
+
+    $query = $mysqli->prepare('select id
+    from teachers 
+    where class_id=?');
+    $query->bind_param('i', $class_id);
+    $query->execute();
+    $query->store_result();
+    $query->bind_result($class_id2);
+    $query->fetch();
+    $num_rows3 = $query->num_rows();
+
+
+    if($num_rows2 == 0 && $num_rows3 ){
+        
     $query = $mysqli->prepare('insert into students(class_id,user_id)  values(?,?)');
     $query->bind_param('ii', $class_id, $user_id);
     $query->execute();
-
     $response['status'] = "success";
     $response['class_id'] = $class_id;
     $response['user_id '] = $user_id;
+    
+    }else{
+        $response['status'] = "You are already in class"
+    }
+
+
+
+
+
+
+
+
 }
 
 echo json_encode($response);
