3a85d3c9e5ce4b9af4277069592932d68655646b
najm-choueiry <nachoueiry@outlook.com>
2023-07-23 16:45:51 +0300

    create_classroom

--- frontend/js/classroom_view.js
+++ frontend/js/classroom_view.js
@@ -18,6 +18,9 @@ body.addEventListener("click", function(event) {
 const class_options = document.getElementById('class_options')
 const class_options_list = document.getElementById('class_options1')
 
+
+const base_url = "http://localhost/Assignments/google-classroom-clone/backend/";
+
 class_options.addEventListener('click', function(){
   if (class_options_list.style.display == "none") {
     class_options_list.style.display = "block";
@@ -28,17 +31,55 @@ class_options.addEventListener('click', function(){
 
 const join_class = document.getElementById('join_class')
 const create_class = document.getElementById('create_class')
+const create_class_requirements = document.getElementById('create_class_requirements')
+
+// join_class.addEventListener('click', joinClass)
+create_class.addEventListener('click', function(){
+  if (create_class_requirements.style.display == "none") {
+    create_class_requirements.style.display = "block";
+  } else {
+    create_class_requirements.style.display = "none";
+  }
+})
 
 
-join_class.addEventListener('click', joinClass)
-create_class.addEventListener('click', createClass)
 
+const submit_class_info = document.getElementById('submit_class_info')
 
-const joinClass = ()=> {
-  
-}
 
 
 const createClass = ()=> {
-  
+  const name = document.getElementById('name').value
+  const section = document.getElementById('section').value
+  const subject = document.getElementById('subject').value
+  const room = document.getElementById('room').value
+  const id = localStorage.getItem('user_id')
+
+
+  const create_class_form = new FormData()
+
+  create_class_form.append("name", name)
+  create_class_form.append("section", section)
+  create_class_form.append("subject", subject)
+  create_class_form.append("room", room)
+  create_class_form.append("user_id", id)
+
+
+  fetch(base_url + "create_classroom.php",{
+    method: "POST",
+    body: create_class_form
+})
+.then((res) => res.json())
+.then((data) => {
+
+if (data.status  == "success") {
+  window.location.replace("../views/classroom_stream.html")
+    
+}else{
+    console.log('error')
 }
+})
+}
+
+
+submit_class_info.addEventListener('click', createClass)
