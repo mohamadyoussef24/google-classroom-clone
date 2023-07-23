392a50b32f78b1885435d7fe5d38ca160d6d3059
zeinaCodes <eye_likemoon@hotmail.com>
2023-07-23 14:31:51 +0300

    assignments listing in js

--- frontend/js/assignment.js
+++ frontend/js/assignment.js
@@ -1,5 +1,8 @@
+
+const pages = {}
+
 // function to show dropdowns on assignment creation page
-async function showDropdowns() {
+pages.showDropdowns = async() => {
 
     let arrow2 = document.getElementById("arrow2")
     let class_ul = document.getElementById("class-ul")
@@ -40,7 +43,7 @@ function getChecked() {
 }
 
 // function to create assignment
-function createAssignment() {
+pages.createAssignment = () => {
     let assign_btn = document.getElementById("create_assignment");
     assign_btn.addEventListener('click', async function (e) {
         e.preventDefault()
@@ -78,5 +81,41 @@ function createAssignment() {
     })
 }
 
-showDropdowns();
-createAssignment();
+function displayAssignments(assignments_array) {
+    const assignments_list = document.getElementById("assignments-ul");
+    assignments_list.innerHTML = "";
+    assignments_array.forEach((assignment) => {
+        let assignment_li = document.createElement("li");
+        assignment_li.innerHTML = `
+        <div class="li-title" id="li-title"><svg focusable="false" width="24" height="24"
+                    viewBox="0 0 24 24" class=" NMm5M hhikbc svg-blue">
+                    <path d="M7 15h7v2H7zm0-4h10v2H7zm0-4h10v2H7z"></path>
+                    <path
+                        d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-.14 0-.27.01-.4.04a2.008 2.008 0 0 0-1.44 1.19c-.1.23-.16.49-.16.77v14c0 .27.06.54.16.78s.25.45.43.64c.27.27.62.47 1.01.55.13.02.26.03.4.03h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7-.25c.41 0 .75.34.75.75s-.34.75-.75.75-.75-.34-.75-.75.34-.75.75-.75zM19 19H5V5h14v14z">
+                    </path>
+                </svg><span>${assignment.title}</span></div>
+                <div class="due-date">${assignment.due}</div>
+    `;
+        assignments_list.appendChild(assignment_li)
+        assignment_li.classList.add("assignment-li")
+    })
+}
+
+// get assigmnets
+pages.getAssignments = () => {
+    // fetch("https://google-classroom-backend/get_assignment.php")
+    //     .then((response) => response.json())
+    //     .then((assignments) => {
+    //         assignments_array = assignments;
+    //         displayAssignments();
+    //     })
+    //     .catch((error) => console.log(error))
+let assignments_array = [{ title: "assignment", due: "23-34 -2023" }, { title: "ehudh", due: "346-30-34" }]
+    displayAssignments(assignments_array);
+}
+
+//this will load the scripts of the mentioned page
+pages.loadFor = (func_name) => {
+    eval("pages." + func_name + "();")
+}
+
