8cb55aee7f52912b364ffc3e0cd262d4b55d86a5
Alaa Attaya <attayaalaa@gmail.com>
2023-07-27 01:56:45 +0300

    Final commit?

--- frontend/js/testing_accept_cancel.js
+++ frontend/js/testing_accept_cancel.js
@@ -19,16 +19,42 @@ window.onload = function() {
     console.log(get_code)
     console.log(email_sent)
 
-    if (get_code.trim() == ""||email_sent.trim()==""){
+    if (get_code.trim() == "" || email_sent.trim()==""){
         window.location.replace('/index.html')
     }else{
 
     }
     //// join class as student////
     
-    // window.location.replace(url + "?code=" + get_code);
 
+    try {
+    const checkclass_form = new FormData()
+    checkclass_form.append("email", email_sent)
+    checkclass_form.append("class_code", get_code)
 
+        fetch(base_url + 'Check_user_class.php', {
+            method: "POST",
+            body: checkclass_form
+        })
+        .then((res) => res.json())
+        .then((data) => {
+            result = data.status
+            console.log(result)
+        
+            localStorage.setItem("user_id", data.user_id)
+            
+        
+            window.location.replace(url + "?code=" + get_code);
+        })
+        
+
+    .catch((err) => {
+        console.log("Fetch error:", err);
+    });
+ } catch (err) {
+    console.log("Error:", err);
+
+  }
+  
 
 }
-
