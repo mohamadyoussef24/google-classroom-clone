00ac2c9451ae35afafdc9125cff78272c5a7009a
Alaa Attaya <attayaalaa@gmail.com>
2023-07-26 20:02:47 +0300

    Fix some styling

--- backend/student_upload.php
+++ backend/student_upload.php
@@ -9,7 +9,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             
             $class_code = $_POST['class_code']; 
 
-
+            $assignment_id= $_POST['assignment_id'];
+            
             $query = $mysqli->prepare('select id
 from classes 
 where class_code=?');
@@ -20,8 +21,18 @@ $query->store_result();
 $query->bind_result($class_id);
 $query->fetch();
 
-            $id = $_POST['user_id']; 
-            $dir = "./classes/class_$class_id/student_$id/";
+            
+            $query = $mysqli->prepare('select id
+from students
+where class_id=?');
+$query->bind_param('i', $class_id);
+$query->execute();
+
+$query->store_result();
+$query->bind_result($students_id);
+$query->fetch();
+            
+            $dir = "./classes/class_$class_id/student_$students_id/";
 
             if (!file_exists($dir)) {
                 if (mkdir($dir, 0755, true)) {
@@ -33,7 +44,7 @@ $query->fetch();
                 echo "Directory already exists.";
             }
             
-
+            
         // Loop through each uploaded file
         foreach ($_FILES['uploadedFiles']['name'] as $index => $fileName) {
             $tempFilePath = $_FILES['uploadedFiles']['tmp_name'][$index];
@@ -44,6 +55,11 @@ $query->fetch();
                 $destination = $dir . $fileName;
                 // Move the file to the desired path on the server
                 if (move_uploaded_file($tempFilePath, $destination)) {
+
+                    $query = $mysqli->prepare('insert into files (assignment_id,student_id,file_path) values(?,?,?)');
+                    $query->bind_param('iis',  $assignment_id, $student_id, $destination);
+                    $query->execute();
+                    
                     echo "File {$fileName} was uploaded successfully.\n";
                 } else {
                     echo "Error moving file {$fileName} to the server.\n";
--- frontend/js/stream.js
+++ frontend/js/stream.js
@@ -418,3 +418,21 @@ function myFunction() {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+const logout = document.getElementById('logout')
+logout.addEventListener('click', function () {
+  localStorage.removeItem("user_id")
+  localStorage.removeItem("email")
+  localStorage.removeItem("class_code")
+  window.location.replace('../views/signin.html')
+})
